90fe04dfd5f3970a6e852f8cdac48f5ac91e9326
Ambre Palanque <ambre.palanque@hotmail.com>
2018-06-28 14:22:33 +0200

    newVersion

--- new/js/main.js
+++ new/js/main.js
@@ -1,73 +1,60 @@
 var app = new PIXI.Application({backgroundColor : 0xffffff});
 document.body.appendChild(app.view);
 
-// holder to store the aliens
-var aliens = [];
+var traits = [];
 
-var totalDudes = 30;
+var totalTraits = 30;
 
-for (var i = 0; i < totalDudes; i++) {
+for (var i = 0; i < totalTraits; i++) {
 
-    // create a new Sprite that uses the image name that we just generated as its source
-    var dude =  PIXI.Sprite.fromImage('img/trait.png');
+    var trait =  PIXI.Sprite.fromImage('img/trait.png');
 
-    // set the anchor point so the texture is centerd on the sprite
-    dude.anchor.set(0.5);
+    trait.anchor.set(0.5);
 
-    // set a random scale for the dude - no point them all being the same size!
-    dude.scale.set(0.8 + Math.random() * 0.3);
+    trait.scale.set(0.8 + Math.random() * 0.3);
 
-    // finally lets set the dude to be at a random position..
-    dude.x = Math.random() * app.screen.width;
-    dude.y = Math.random() * app.screen.height;
+    trait.x = Math.random() * app.screen.width;
+    trait.y = Math.random() * app.screen.height;
 
-    // create some extra properties that will control movement :
-    // create a random direction in radians. This is a number between 0 and PI*2 which is the equivalent of 0 - 360 degrees
-    dude.direction = Math.random() * Math.PI * 2;
+    trait.direction = Math.random() * Math.PI * 2;
 
-    // this number will be used to modify the direction of the dude over time
-    dude.turningSpeed = Math.random() - 0.8;
+    trait.turningSpeed = Math.random() - 0.8;
 
-    // create a random speed for the dude between 0 - 2
-    dude.speed = 0.1 + Math.random() * 0.1;
+    trait.speed = 0.1 + Math.random() * 0.1;
 
-    // finally we push the dude into the aliens array so it it can be easily accessed later
-    aliens.push(dude);
+    traits.push(trait);
 
-    app.stage.addChild(dude);
+    app.stage.addChild(trait);
 }
 
-// create a bounding box for the little dudes
-var dudeBoundsPadding = 100;
-var dudeBounds = new PIXI.Rectangle(-dudeBoundsPadding,
-                                    -dudeBoundsPadding,
-                                    app.screen.width + dudeBoundsPadding * 2,
-                                    app.screen.height + dudeBoundsPadding * 2);
+var traitBoundsPadding = 100;
+var traitBounds = new PIXI.Rectangle(-traitBoundsPadding,
+                                    -traitBoundsPadding,
+                                    app.screen.width + traitBoundsPadding * 2,
+                                    app.screen.height + traitBoundsPadding * 2);
 
 app.ticker.add(function() {
 
-    // iterate through the dudes and update their position
-    for (var i = 0; i < aliens.length; i++) {
+    for (var i = 0; i < traits.length; i++) {
 
-        var dude = aliens[i];
-        dude.direction += dude.turningSpeed * 0.01;
-        dude.x += Math.sin(dude.direction) * dude.speed;
-        dude.y += Math.cos(dude.direction) * dude.speed;
-        dude.rotation = -dude.direction - Math.PI / 2;
+        var trait = traits[i];
+        trait.direction += trait.turningSpeed * 0.01;
+        trait.x += Math.sin(trait.direction) * trait.speed;
+        trait.y += Math.cos(trait.direction) * trait.speed;
+        trait.rotation = -trait.direction - Math.PI / 2;
 
-        // wrap the dudes by testing their bounds...
-        if (dude.x < dudeBounds.x) {
-            dude.x += dudeBounds.width;
+        if (trait.x < traitBounds.x) {
+            trait.x += traitBounds.width;
         }
-        else if (dude.x > dudeBounds.x + dudeBounds.width) {
-            dude.x -= dudeBounds.width;
+        else if (trait.x > traitBounds.x + traitBounds.width) {
+            trait.x -= traitBounds.width;
         }
 
-        if (dude.y < dudeBounds.y) {
-            dude.y += dudeBounds.height;
+        if (trait.y < traitBounds.y) {
+            trait.y += traitBounds.height;
         }
-        else if (dude.y > dudeBounds.y + dudeBounds.height) {
-            dude.y -= dudeBounds.height;
+        else if (trait.y > traitBounds.y + traitBounds.height) {
+            trait.y -= traitBounds.height;
         }
     }
 });
