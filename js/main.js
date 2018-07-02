7377f5aae6b1dd470bc0915fd66f2b2c5bc44956
Ambre Palanque <ambre.palanque@hotmail.com>
2018-07-02 19:59:59 +0200

    actu site

--- js/main.js
+++ js/main.js
@@ -82,6 +82,7 @@ $(function(){
 
         window.setInterval(function(){
             $('#formImg').attr('src',firstArray[thisId]);
+            console.log(firstArray[thisId]);
             $('#expImg').attr('src',secondArray[thisId]);
             $('#compImg').attr('src',thirdArray[thisId]);
             thisId++; //increment data array id
